0c41978fe084a158ce2d93e6f561cdb3a3a73f0f
Dan Whitford <daniel.whitford@roke.co.uk>
2018-06-15 11:22:27 +0100

    Add class for main method and class for utils.

--- src/main/java/scraper/Scraper.java
+++ src/main/java/scraper/Scraper.java
@@ -2,70 +2,25 @@ package scraper;
 
 import model.Product;
 import model.Results;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Scraper {
 
-    private static Double getDoubleFromString(String s) {
-        s = s.split(" ")[0];
-        String cleanString = s.replaceAll("[^0-9.]", "");
-        return Double.parseDouble(cleanString);
-    }
-
-    private static Product processProductElement(Element product, String baseUrl) throws IOException {
-        Element linkTo = product.selectFirst("a");
-        String title = product.selectFirst("a").text();
-        String pricePerUnitString = product.selectFirst("p.pricePerUnit").text();
-        Double pricePerUnit = getDoubleFromString(pricePerUnitString);
-
-        String extraInfoLink = linkTo.attr("href");
-        extraInfoLink = (new URL( new URL(baseUrl), extraInfoLink)).toString();
-        Document detailSoup = Jsoup.connect(extraInfoLink).get();
-        Element infoElement  = detailSoup.getElementById("information");
-        String description = infoElement.selectFirst(".productText").text();
-
-        Element nutritionTable = detailSoup.selectFirst(".nutritionTable");
-        Double kCalPer100g = null;
-        if (nutritionTable != null) {
-            Element calorieCell = nutritionTable.selectFirst("table > tbody > tr:nth-child(2) > td:nth-child(1)");
-            if (calorieCell != null) {
-                String calorieString = calorieCell.text();
-                kCalPer100g = getDoubleFromString(calorieString);
-            }
-        }
-
-        return new Product(title, description, pricePerUnit, kCalPer100g);
-    }
-
-    private static List<Product> scrapeProductsFromUrl(String url) throws IOException {
-        List<Product> productsList = new ArrayList<>();
-        Document soup = Jsoup.connect(url).get();
-        Elements products = soup.getElementsByClass("product");
-
-        for(Element productElement : products) {
-            productsList.add( processProductElement(productElement, url) );
-        }
-        return productsList;
-    }
+    private static final Logger LOGGER = Logger.getLogger( ScraperUtils.class.getName() );
 
     public static void main(String... s) {
         String url = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
         try {
-            List<Product> products = Scraper.scrapeProductsFromUrl(url);
+            List<Product> products = ScraperUtils.scrapeProductsFromUrl(url);
             Results results = new Results(products);
             System.out.println(results);
 
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "Failed to read from url: " + url);
         }
     }
 }
---
+++ src/main/java/scraper/ScraperUtils.java
@@ -0,0 +1,58 @@
+package scraper;
+
+import model.Product;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ScraperUtils {
+
+    private static Double getDoubleFromString(String s) {
+        s = s.split(" ")[0];
+        String cleanString = s.replaceAll("[^0-9.]", "");
+        return Double.parseDouble(cleanString);
+    }
+
+    private static Product processProductElement(Element product, String baseUrl) throws IOException {
+        Element linkTo = product.selectFirst("a");
+        String title = product.selectFirst("a").text();
+        String pricePerUnitString = product.selectFirst("p.pricePerUnit").text();
+        Double pricePerUnit = getDoubleFromString(pricePerUnitString);
+
+        String extraInfoLink = linkTo.attr("href");
+        extraInfoLink = (new URL( new URL(baseUrl), extraInfoLink)).toString();
+        Document detailSoup = Jsoup.connect(extraInfoLink).get();
+        Element infoElement  = detailSoup.getElementById("information");
+        String description = infoElement.selectFirst(".productText").text();
+
+        Element nutritionTable = detailSoup.selectFirst(".nutritionTable");
+        Double kCalPer100g = null;
+        if (nutritionTable != null) {
+            Element calorieCell = nutritionTable.selectFirst("table > tbody > tr:nth-child(2) > td:nth-child(1)");
+            if (calorieCell != null) {
+                String calorieString = calorieCell.text();
+                kCalPer100g = getDoubleFromString(calorieString);
+            }
+        }
+
+        return new Product(title, description, pricePerUnit, kCalPer100g);
+    }
+
+    public static List<Product> scrapeProductsFromUrl(String url) throws IOException {
+        List<Product> productsList = new ArrayList<>();
+        Document soup = Jsoup.connect(url).get();
+        Elements products = soup.getElementsByClass("product");
+
+        for(Element productElement : products) {
+            productsList.add( processProductElement(productElement, url) );
+        }
+        return productsList;
+    }
+}
